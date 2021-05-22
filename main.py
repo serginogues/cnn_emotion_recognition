30e72a59d9f68f9552c2204b053beab2da3e63b5
Simone <grassi.simone98@gmail.com>
2021-05-22 13:55:39 +0200

    Cuda bug solved

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 Emotion Recognition with a CNN
 """
 from cnn import train, test, visualize_filter, model, print_architecture
+from config import *
 
 if __name__ == '__main__':
 
-    # visualize_filter(model.cpu())
-    # print_architecture(model.cpu())
+    visualize_filter(model.cpu())
+    print_architecture(model.cpu())
+    model.to(device)
 
     train()
     test()
